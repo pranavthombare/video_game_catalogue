2918a5af7e9faa6d19b8d015822c7615b678c01c
Pranav Thombare <pranavthombare97@gmail.com>
2018-12-12 12:41:32 +0000

    Initial commit

---
+++ video_game_catalogue.cpp
@@ -0,0 +1,48 @@
+/* -------------------------------------------
+            ECS793P-Coursework
+        Title- Video game catalogue
+          Author- Pranav Thombare
+--------------------------------------------*/
+
+#include<iostream>
+#include<cstdlib>
+#include<random>
+using namespace std;
+
+class Dice{
+
+};
+
+class Cube:public Dice{
+public:
+
+};
+
+class Octahedron:public Dice
+{
+
+};
+
+int main() {
+ /*Dice * d[] = {new Cube(), new Octahedron()};
+ for (int i = 0; i < 2; i++) {
+ Dice* dp = d[i];
+ cout<< (i==0?"Cube:":"Octahedron:") << endl;
+ cout<< "Number of Sides: " << dp->getNumberOfSides() << endl;
+ cout<< "Initial NumRolled: " << dp->getNumberRolled() << endl;
+ dp->roll();
+ cout<< "Roll - NumRolled: " << dp->getNumberRolled() << endl;
+ dp->cheatRoll(true);
+ cout<< "Shake true - NumRolled: " << dp->getNumberRolled() << endl;
+ dp->cheatRoll(false);
+ cout << "Shake false - NumRolled: " << dp->getNumberRolled() << endl;
+ cout<<endl;
+}*/
+
+
+
+
+
+
+
+}
