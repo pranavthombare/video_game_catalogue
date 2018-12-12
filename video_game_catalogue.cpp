3dcf27dbac4a3bb6f747d528070d70a320739db0
Pranav Thombare <pranavthombare97@gmail.com>
2018-12-12 13:05:59 +0000

    cpp: Adjust with the project

--- video_game_catalogue.cpp
+++ video_game_catalogue.cpp
@@ -4,45 +4,53 @@
           Author- Pranav Thombare
 --------------------------------------------*/
 
+/* -------------------------------------------
+            ECS793P-Coursework
+        Title- Video game catalogue
+          Author- Pranav Thombare
+--------------------------------------------*/
+
 #include<iostream>
 #include<cstdlib>
-#include<random>
+#include<string>
+
 using namespace std;
 
-class Dice{
-
-};
-
-class Cube:public Dice{
+class games{
+  int rel_year;
+  string manufacturer;
+  char genre;
+  char rating;
 public:
 
 };
 
-class Octahedron:public Dice
+class console:public games{
+  int size;
+  char version;
+  char console_name;
+
+public:
+
+};
+
+class pc:public games
 {
+  int size;
+  char version;
+  char os;
+public:
+};
+
+class mobile:public games
+{
+  int size;
+  char version;
+  char os;
+public:
 
 };
 
 int main() {
- /*Dice * d[] = {new Cube(), new Octahedron()};
- for (int i = 0; i < 2; i++) {
- Dice* dp = d[i];
- cout<< (i==0?"Cube:":"Octahedron:") << endl;
- cout<< "Number of Sides: " << dp->getNumberOfSides() << endl;
- cout<< "Initial NumRolled: " << dp->getNumberRolled() << endl;
- dp->roll();
- cout<< "Roll - NumRolled: " << dp->getNumberRolled() << endl;
- dp->cheatRoll(true);
- cout<< "Shake true - NumRolled: " << dp->getNumberRolled() << endl;
- dp->cheatRoll(false);
- cout << "Shake false - NumRolled: " << dp->getNumberRolled() << endl;
- cout<<endl;
-}*/
-
-
-
-
-
-
 
 }
