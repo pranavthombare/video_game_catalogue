f8cfe1dc3b7fdd7262f55b830754d69b9f4589f3
Pranav Thombare <pranavthombare97@gmail.com>
2018-12-15 21:11:01 +0000

    cpp: Added switch case and refine code

--- video_game_catalogue.cpp
+++ video_game_catalogue.cpp
@@ -4,15 +4,12 @@
           Author- Pranav Thombare
 --------------------------------------------*/
 
-/* -------------------------------------------
-            ECS793P-Coursework
-        Title- Video game catalogue
-          Author- Pranav Thombare
---------------------------------------------*/
-
 #include<iostream>
+#include<fstream>
 #include<cstdlib>
 #include<string>
+
+#define FILE_NAME = "video_game_catalog.txt";
 
 using namespace std;
 
@@ -20,8 +17,20 @@ class games{
   int rel_year;
   string manufacturer;
   char genre;
-  char rating;
+  char rating;            //Used for ESRB Ratings
 public:
+
+  //Getter Methods
+  void getReleaseYear();
+  void getManufacturer();
+  void getGenre();
+  void getRating();
+
+  //Setter Methods
+  void setReleaseYear();
+  void setManufacturer();
+  void setGenre();
+  void setRating();
 
 };
 
@@ -29,7 +38,6 @@ class console:public games{
   int size;
   char version;
   char console_name;
-
 public:
 
 };
@@ -40,6 +48,7 @@ class pc:public games
   char version;
   char os;
 public:
+
 };
 
 class mobile:public games
@@ -51,6 +60,48 @@ public:
 
 };
 
-int main() {
+int main()
+{
+
+  int choice;
+//  char ;
+
+  system("clear");
+  cout<<"\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t\t\tWelcome to Video game inventory\n\n\n\n\n\n\n";
+  cout<<"What would you like to do?\n";
+  cout<<"1. Search \n2. Edit \n3. Delete \n4. Insert \n5. Quit";
+  cin>>choice;
+
+  switch(choice)
+  {
+    case 1: system("clear");            //Used to clear the terminal window
+            int search_choice;
+            cout<<"Search by : \n1.Game Title \n2.Production house(EA, Ubisoft, Activision etc) \n3.All";
+            cin>>search_choice;
+            switch(search_choice)
+            {
+              case 1:
+              break;
+              case 2:
+              break;
+              case 3:
+              break;
+            }
+    break;
+
+    case 2: system("clear");
+            
+    break;
+
+    case 4: system("clear");
+
+    break;
+
+    case 5: system("clear");
+            cout<<"\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t\t\tThank you\n\n\n\n\n\n\n\n"
+    break;
+
+    default: break;
+  }
 
 }
