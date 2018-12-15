8d51abb0f67fd587b787af60253d4902a547c8f1
Pranav Thombare <pranavthombare97@gmail.com>
2018-12-15 21:23:56 +0000

    cpp: Add more logic to case 1.

--- video_game_catalogue.cpp
+++ video_game_catalogue.cpp
@@ -64,11 +64,14 @@ int main()
 {
 
   int choice;
-//  char ;
+  string game_title;
+  string production_house;
+
 
   system("clear");
   cout<<"\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t\t\tWelcome to Video game inventory\n\n\n\n\n\n\n";
   cout<<"What would you like to do?\n";
+  comeback:
   cout<<"1. Search \n2. Edit \n3. Delete \n4. Insert \n5. Quit";
   cin>>choice;
 
@@ -76,32 +79,39 @@ int main()
   {
     case 1: system("clear");            //Used to clear the terminal window
             int search_choice;
-            cout<<"Search by : \n1.Game Title \n2.Production house(EA, Ubisoft, Activision etc) \n3.All";
+            cout<<"Search by : \n1. Game Title \n2. Production house(EA, Ubisoft, Activision etc) \n3. All \n4. Go to previous menu";
             cin>>search_choice;
             switch(search_choice)
             {
-              case 1:
+              case 1: system("clear");
+                      cout<<"\n Enter the game title you want to search: ";
+                      cin>>game_title;
               break;
-              case 2:
+
+              case 2: system("clear");
+                      cout<<"\n Enter the game title you want to search: ";
+                      cin>>production_house;
               break;
-              case 3:
+
+              case 3: system("clear");
+              break;
+
+              case 4: system("clear");
+                      goto comeback;
               break;
             }
     break;
 
     case 2: system("clear");
-            
     break;
 
     case 4: system("clear");
-
     break;
 
     case 5: system("clear");
-            cout<<"\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t\t\tThank you\n\n\n\n\n\n\n\n"
+            cout<<"\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t\t\tThank you\n\n\n\n\n\n\n\n";
     break;
 
-    default: break;
   }
 
 }
